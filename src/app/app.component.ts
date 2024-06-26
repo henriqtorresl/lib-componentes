6a22eabef633cfcb4a65b5a2e5689605409fa283
Henrique Torres <henriquetlandin@gmail.com>
2024-06-26 16:15:05 -0300

    componente de multiselect -autocomplete finalizado e dinamico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable, map, startWith } from 'rxjs';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MultiselectAutocompleteComponent } from './components/multiselect-autocomplete/multiselect-autocomplete.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+    MultiselectAutocompleteComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'lib-componentes';
+export class AppComponent implements OnInit {
+  options: string[] = ['One', 'Two', 'Three'];
+  selectedOptions: string[] = [];
+
+  constructor() {}
+ 
+  ngOnInit() {
+  }
+
+  updateForm(values: any) {
+    this.selectedOptions = values;
+  }
+
 }
